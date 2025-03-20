ebbb30dfa03891816ca297970ce8d2b0fc20a3a3
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2025-03-20 23:33:09 +0700

    feat: add env

--- config/database.js
+++ config/database.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
+
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('lustrumdtsl', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: console.log,
-})
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: console.log,
+  }
+)
 
 module.exports = sequelize
