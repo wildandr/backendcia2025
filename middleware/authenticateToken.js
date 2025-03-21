de2886301939edcf9a02d829129bb79656007da1
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2025-03-22 01:35:13 +0700

    fix : upload files multer

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 
-const JWT_SECRET = process.env.JWT_SECRET || 'backendCIA'
+const JWT_SECRET = process.env.JWT_SECRET || 'backendcia2025'
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization']
