661283d10479b70755109564243955bb00979fea
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2025-03-22 13:15:25 +0700

    add specific folder

--- routes/craft.js
+++ routes/craft.js
@@ -10,7 +10,7 @@ const path = require('path')
 // Configure multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads')
+    cb(null, './uploads/craft')
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
